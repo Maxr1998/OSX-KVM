051f15ef3cf0aba7f43625ad9bb6f30e4a9c0f68
Maxr1998 <max.rumpf1998@gmail.com>
2019-02-11 01:52:23 +0100

    Fix paths, remove unused images, use user networking

--- boot-macOS-Mojave.sh
+++ boot-macOS-Mojave.sh
@@ -13,7 +13,7 @@
 
 MY_OPTIONS="+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 
-qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
+qemu-system-x86_64 -enable-kvm -m 6144 -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
 	  -machine pc-q35-2.11 \
 	  -smp 4,cores=2 \
 	  -usb -device usb-kbd -device usb-tablet \
@@ -22,11 +22,7 @@ qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -drive if=pflash,format=raw,file=OVMF_VARS-1024x768.fd \
 	  -smbios type=2 \
 	  -device ich9-intel-hda -device hda-duplex \
-	  -device ide-drive,bus=ide.2,drive=Clover \
-	  -drive id=Clover,if=none,snapshot=on,format=qcow2,file=./'Mojave/Clover.qcow2' \
 	  -device ide-drive,bus=ide.1,drive=MacHDD \
-	  -drive id=MacHDD,if=none,file=./mac_hdd.img,format=qcow2 \
-	  -device ide-drive,bus=ide.0,drive=MacDVD \
-	  -drive id=MacDVD,if=none,snapshot=on,media=cdrom,file=./'Mojave-10.14.4.iso' \
-	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+	  -drive id=MacHDD,if=none,file=/home/max/mac_hdd.nobak.img,format=qcow2 \
+	  -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
 	  -monitor stdio
